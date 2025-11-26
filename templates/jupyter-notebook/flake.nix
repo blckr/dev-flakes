8fed53d7d0b93f294d222f43c09bcbc402771ff0
Arved Blöcker <contact@arvedbloecker.de>
2025-11-26 16:47:39 +0100

    Jupyter Notebook guide to set empty token

--- templates/jupyter-notebook/flake.nix
+++ templates/jupyter-notebook/flake.nix
@@ -26,7 +26,7 @@
       devShells = forEachSupportedSystem (
         { pkgs }:
         {
-          default = pkgs.mkShellNoCC {
+          default = pkgs.mkShell {
             venvDir = ".venv";
             packages =
               with pkgs;
@@ -42,10 +42,24 @@
                 pip
                 venvShellHook
               ]);
+            
+            JUPYTER_CONFIG_DIR = ".jupyter";
+            JUPYTER_DATA_DIR = ".jupyter/share";
+            JUPYTER_RUNTIME_DIR = ".jupyter/runtime";
+            
+            JUPYTERLAB_SETTINGS_DIR = ".jupyter/lab/user-settings";
+            JUPYTERLAB_SCHEMAS_DIR = ".jupyter/lab/schemas";
+            
             shellHook = ''
+              unset PYTHONPATH
+              
+              mkdir -p .jupyter/lab/user-settings
+              mkdir -p .jupyter/lab/schemas
+              mkdir -p .jupyter/runtime
+              
               echo ""
               echo " -------- Starting Jupyter Notebook --------"
-              echo "jupyter notebook <filename>"
+              echo "jupyter notebook --NotebookApp.token='' <filename>"
               echo ""
             '';
           };
