0a88d7ed49db5cbbb875dd990790247668ba2860
Arved Blöcker <contact@arvedbloecker.de>
2025-12-10 15:17:29 +0100

    :bug: Fixed Jupyter Notebook Flake

--- templates/jupyter-notebook/flake.nix
+++ templates/jupyter-notebook/flake.nix
@@ -1,69 +1,41 @@
 {
-  description = "Jupyter-Flake by blckr";
+  description = "Jupyter Dev-Flake by blckr";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs =
-    { self, ... }@inputs:
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        
+        # Create Python environment with JupyterLab and extensions
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          jupyterlab
+          ipykernel
+          ipywidgets
+          notebook
+          jupyterlab-git
+          ipympl
+        ]);
 
-    let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import inputs.nixpkgs { inherit system; };
-          }
-        );
-    in
-    {
-      devShells = forEachSupportedSystem (
-        { pkgs }:
-        {
-          default = pkgs.mkShell {
-            venvDir = ".venv";
-            packages =
-              with pkgs;
-              [
-                poetry
-                python311
-              ]
-              ++ (with python311Packages; [
-                jupyter
-                notebook
-                jupyterlab
-                ipykernel
-                pip
-                venvShellHook
-              ]);
-            
-            JUPYTER_CONFIG_DIR = ".jupyter";
-            JUPYTER_DATA_DIR = ".jupyter/share";
-            JUPYTER_RUNTIME_DIR = ".jupyter/runtime";
-            
-            JUPYTERLAB_SETTINGS_DIR = ".jupyter/lab/user-settings";
-            JUPYTERLAB_SCHEMAS_DIR = ".jupyter/lab/schemas";
-            
-            shellHook = ''
-              unset PYTHONPATH
-              
-              mkdir -p .jupyter/lab/user-settings
-              mkdir -p .jupyter/lab/schemas
-              mkdir -p .jupyter/runtime
-              
-              echo ""
-              echo " -------- Starting Jupyter Notebook --------"
-              echo "jupyter notebook --NotebookApp.token=\'\' <filename>"
-              echo ""
-            '';
-          };
-        }
-      );
-    };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pythonEnv
+            pkgs.nodejs
+          ];
+
+          shellHook = ''
+            echo "JupyterLab environment ready!"
+            echo ""
+            echo "Start with: jupyter lab"
+          '';
+        };
+      }
+    );
 }
+
